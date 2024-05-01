6765d3083993c53491e0feb40e991374b29425cc
harshveer7828 <harshveer7828@gmail.com>
2024-05-01 14:08:44 +0530

    update messages again

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 
-const plm=require('passport-local-mongoose')
+const plm=require('passport-local-mongoose');
+const { Socket } = require('socket.io');
 
 mongoose.connect('mongodb+srv://harshveer7828:0111Al211068@cluster0.ppnm2pu.mongodb.net/');
 // mongoose.connect('mongodb://0.0.0.0/Insatgram');
@@ -38,6 +39,7 @@ const userSchema=mongoose.Schema({
       ref:"post"
     }
   ],
+  socketId:String
 })
 userSchema.plugin(plm);
 
--- socketapi.js
+++ socketapi.js
@@ -8,17 +8,36 @@ const socketapi = {
 // Add your socket.io logic here!
 io.on("connection", function (socket) {
     console.log("A user connected");
-    socket.on('sent-private-message', async data => {
-        const messageCreate = await messageModel.create({
-            sender: data.sender,
-            data: data.message,
-            reciever: data.reciever
-        });
-        // await userModel.findOne({ username: data.reciever });
-        socket.broadcast.emit('recieve-private-message', data);
+
+    // Handle 'join-server' event
+    socket.on('join-server', async (username) => {
+        try {
+            // Update socketId in userModel for the connected user
+            await userModel.findOneAndUpdate(
+                { username: username },
+                { socketId: socket.id }
+            );
+        } catch (error) {
+            console.log("Error updating socketId:", error.message);
+        }
     });
 
-    
+    // Handle 'sent-private-message' event
+    socket.on('sent-private-message', async (data) => {
+        try {
+            // Save the message in messageModel
+            const message = await messageModel.create({
+                sender: data.sender,
+                message: data.message,
+                receiver: data.receiver
+            });
+
+            // Emit 'recieve-private-message' event to the receiver's socketId
+            io.to(data.receiver).emit('recieve-private-message', { message: data.message });
+        } catch (error) {
+            console.log("Error sending private message:", error.message);
+        }
+    });
 
 
 
